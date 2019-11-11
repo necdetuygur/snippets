83cd70406f1034aa7363b6aa6c3e72258d840464
necdetuygur <necdet.uygur@gmail.com>
2019-11-11 14:13:04 +0300

    Create SchTasks Crate & Delete.bat

---
+++ SchTasks Crate & Delete.bat
@@ -0,0 +1,12 @@
+SchTasks /Create /SC DAILY /TN "Task 09" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 09:00
+SchTasks /Create /SC DAILY /TN "Task 10" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 10:00
+SchTasks /Create /SC DAILY /TN "Task 11" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 11:00
+SchTasks /Create /SC DAILY /TN "Task 12" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 12:00
+SchTasks /Create /SC DAILY /TN "Task 13" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 13:00
+SchTasks /Create /SC DAILY /TN "Task 14" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 14:00
+SchTasks /Create /SC DAILY /TN "Task 15" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 15:00
+SchTasks /Create /SC DAILY /TN "Task 16" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 16:00
+SchTasks /Create /SC DAILY /TN "Task 17" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 17:00
+SchTasks /Create /SC DAILY /TN "Task 18" /TR "C:\Users\User\Drive\BAT\StartPush.bat" /ST 18:00
+
+SchTasks /Delete /TN "Task 18"
