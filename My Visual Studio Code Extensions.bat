87014fa29a46e0da15e708e5e6dc7caae412100e
necdetuygur <necdet.uygur@gmail.com>
2019-11-19 09:16:17 +0300

    Update My Visual Studio Code Extensions.bat

--- My Visual Studio Code Extensions.bat
+++ My Visual Studio Code Extensions.bat
@@ -12,3 +12,17 @@ code --install-extension mrmlnc.vscode-apache
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension wmaurer.change-case
 code --install-extension zhuangtongfa.Material-theme
+
+code --install-extension Atishay-Jain.All-Autocomplete
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension eamodio.gitlens
+code --install-extension felixfbecker.php-debug
+code --install-extension felixfbecker.php-intellisense
+code --install-extension HookyQR.beautify
+code --install-extension mhutchie.git-graph
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension mtxr.sqltools
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension zhuangtongfa.material-theme
