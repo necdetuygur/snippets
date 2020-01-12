e792cd851da1bdfb5faae38a10dc2487a0d42f41
necdetuygur <necdet.uygur@gmail.com>
2020-01-13 01:02:41 +0300

    Update termux-ytdl-ffmpeg.sh

--- termux-ytdl-ffmpeg.sh
+++ termux-ytdl-ffmpeg.sh
@@ -1,14 +1,18 @@
-pkg install proot
-termux-chroot
 apt update
 apt install golang
 apt install git
+pkg install ffmpeg
+pkg install proot
+termux-chroot
+
 wget https://yt-dl.org/downloads/latest/youtube-dl -O ./youtube-dl
 cp -r youtube-dl /usr/bin/
+chmod a+rx /usr/bin/youtube-dl
+
 git clone http://github.com/necdetuygur/yt-dl
 cd yt-dl/
-go build downloader.go 
+go build downloader.go
 cp -r downloader /usr/bin/
-chmod a+rx /usr/bin/downloader 
-pkg install ffmpeg
+chmod a+rx /usr/bin/downloader
+
 downloader https://m.youtube.com/watch?v=#
