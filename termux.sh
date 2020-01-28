55bfd577bb040cbd7fe9acc4cabe74113df3bc90
necdetuygur <necdet.uygur@gmail.com>
2020-01-28 23:12:34 +0300

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,26 @@
+pkg install -y golang git openssh ffmpeg python nodejs tsu wget curl proot
+
+termux-setup-storage
+termux-chroot
+
+wget https://yt-dl.org/downloads/latest/youtube-dl
+mv youtube-dl /usr/bin/
+chmod a+rx /usr/bin/youtube-dl
+
+git clone https://github.com/necdetuygur/yt-dl
+cd yt-dl
+go build downloader.go
+mv downloader /usr/bin/
+chmod a+rx /usr/bin/downloader
+cd ..
+
+git clone https://github.com/necdetuygur/go-test
+cd go-test
+go build gold.go
+mv gold /usr/bin/
+chmod a+rx /usr/bin/gold
+cd ..
+
+termux-chroot 
+cd storage/downloads
+downloader 480 https://www.youtube.com/watch?v=<VID>
